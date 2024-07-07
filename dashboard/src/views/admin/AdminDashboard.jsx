21aa757e55aeedaca1266bb9411c84bda98d73e0
RubelAzad <rubelazad123@gmail.com>
2024-07-07 17:15:10 +0600

    Dashboard Body Design

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -1,9 +1,185 @@
 import React from 'react';
+import { MdCurrencyExchange, MdProductionQuantityLimits } from 'react-icons/md';
+import { FaUsers } from 'react-icons/fa';
+import { FaCartShopping } from 'react-icons/fa6';
+import Chart from 'react-apexcharts'
+import { Link } from 'react-router-dom';
 
 const AdminDashboard = () => {
+
+    const state = {
+        series: [{
+                name: 'Orders',
+                data: [30, 40, 35, 50, 49, 80, 100]
+            },
+            {
+                name: 'Revenue',
+                data: [30, 40, 20, 55, 49, 65, 70]
+            },
+            {
+                name: 'Sellers',
+                data: [30, 40, 35, 50, 70, 60, 70]
+            }
+        ],
+        options: {
+            colors: ['#BE344A', '#53B679', '#2272C6'],
+            plotOptions: {
+                borderRadius: 30
+            },
+            chart: {
+                background: 'transparent',
+                foreColor: '#f0f0f0',
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show:true,
+                curve: ['smooth', 'straight', 'stepline'],
+                lineCap: 'butt',
+                colors:'#f0f0f0',
+                width: .5,
+                dashArray: 0
+            },
+            xaxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            legend: {
+                position: 'top'
+            },
+            responsive:[
+                {
+                    breakpoint: 565,
+                    yaxis: {
+                        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                    },
+                    options: {
+                        plotOptions: {
+                            bar:{
+                                horizontal: true
+                            }
+                        },
+                        chart:{
+                            height: '550px'
+                        }
+                    }
+
+                }
+            ]
+        }
+    }
+    
+
     return (
-        <div>
-            <h1>Admin Dashboard</h1>
+        <div className='px-2 md:px-7 py-5'>
+            <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7'>
+                <div className='flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5c5c]'>
+                        <h2 className='text-3xl font-bold'>100</h2>
+                        <span className='text-md font-medium'>Total sales this month</span>
+
+                    </div>
+                    <div className='w-[47px] h-[47px] rounded-full bg-[#fa0305] flex justify-center items-center text-xl'>
+                        <MdCurrencyExchange className='text-[#fae8e8] shadow-lg'/>
+                    </div>
+                </div>
+                <div className='flex justify-between items-center p-5 bg-[#fee2e2] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5c5c]'>
+                        <h2 className='text-3xl font-bold'>50</h2>
+                        <span className='text-md font-medium'>Products</span>
+
+                    </div>
+                    <div className='w-[47px] h-[47px] rounded-full bg-[#760077] flex justify-center items-center text-xl'>
+                        <MdProductionQuantityLimits className='text-[#fae8e8] shadow-lg'/>
+                    </div>
+                </div>
+                <div className='flex justify-between items-center p-5 bg-[#e9feea] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5c5c]'>
+                        <h2 className='text-3xl font-bold'>20</h2>
+                        <span className='text-md font-medium'>Sellers</span>
+
+                    </div>
+                    <div className='w-[47px] h-[47px] rounded-full bg-[#51d758] flex justify-center items-center text-xl'>
+                        <FaUsers className='text-[#fae8e8] shadow-lg'/>
+                    </div>
+                </div>
+                
+                <div className='flex justify-between items-center p-5 bg-[#ecebff] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5c5c]'>
+                        <h2 className='text-3xl font-bold'>200</h2>
+                        <span className='text-md font-medium'>Orders</span>
+
+                    </div>
+                    <div className='w-[47px] h-[47px] rounded-full bg-[#0200f8] flex justify-center items-center text-xl'>
+                        <FaCartShopping className='text-[#fae8e8] shadow-lg'/>
+                    </div>
+                </div>
+            </div>
+            <div className='w-full flex flex-wrap mt-7'>
+                <div className='w-full lg:w-7/12 lg:pr-3'>
+                    <div className='w-full bg-[#645ea4] p-4 rounded-md'>
+                        <Chart options={state.options} series={state.series} type="bar" height={350}/>
+                    </div>
+                </div>
+                <div className='w-full lg:w-5/12 lg:pl-4 mt-4 lg:mt-0'>
+                    <div className='w-full bg-[#645ea4] p-4 rounded-md text-[#fae8e8]'>
+                        <div className='flex justify-between items-center'>
+                            <h2 className='text-lg font-semibold text-[#fae8e8]'>Recent Seller Message</h2>
+                            <Link className='px-3 py-1 bg-[#fae8e8] text-[#645ea4] font-semibold rounded-md'>View All</Link>
+                        </div>
+                        <div className='flex flex-col gap-2 pt-6 text-[#5c5c5c]'>
+                            <ol className='relative border-slate-700 ml-4'>
+                                <li className='mb-3 ml-6'>
+                                    <div className='flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4c7fe2] rounded-full z-10'>
+                                        <img className='w-full rounded-full h-full shadow-lg' src='http://localhost:3000/images/admin.jpg' alt=''/>
+                                    </div>
+                                    <div className='p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm'>
+                                        <div className='flex justify-between items-center mb-2'>
+                                            <Link className='text-md font-normal'>Admin</Link>
+                                            <time className='mb-1 text-sm font-normal sm:order-last sm:mb-0'>2 day ago</time>
+                                        </div>
+                                        <div className='p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800'>
+                                            How Are You
+                                        </div>
+                                    </div> 
+                                </li>
+                                <li className='mb-3 ml-6'>
+                                    <div className='flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4c7fe2] rounded-full z-10'>
+                                        <img className='w-full rounded-full h-full shadow-lg' src='http://localhost:3000/images/admin.jpg' alt=''/>
+                                    </div>
+                                    <div className='p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm'>
+                                        <div className='flex justify-between items-center mb-2'>
+                                            <Link className='text-md font-normal'>Admin</Link>
+                                            <time className='mb-1 text-sm font-normal sm:order-last sm:mb-0'>2 day ago</time>
+                                        </div>
+                                        <div className='p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800'>
+                                            How Are You
+                                        </div>
+                                    </div> 
+                                </li>
+                                <li className='mb-3 ml-6'>
+                                    <div className='flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4c7fe2] rounded-full z-10'>
+                                        <img className='w-full rounded-full h-full shadow-lg' src='http://localhost:3000/images/admin.jpg' alt=''/>
+                                    </div>
+                                    <div className='p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm'>
+                                        <div className='flex justify-between items-center mb-2'>
+                                            <Link className='text-md font-normal'>Admin</Link>
+                                            <time className='mb-1 text-sm font-normal sm:order-last sm:mb-0'>2 day ago</time>
+                                        </div>
+                                        <div className='p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800'>
+                                            How Are You
+                                        </div>
+                                    </div> 
+                                </li>
+
+                            </ol>
+                        </div>
+                    </div>
+
+                </div>
+                    
+            </div>
+
         </div>
     );
 };
