44b88d97c064ff56e6eaca3376f7c92b2106a543
RubelAzad <rubelazad123@gmail.com>
2024-07-11 15:21:46 +0600

    De-active Seller page design done

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -4,6 +4,7 @@ const Orders = lazy(()=>import("../../views/admin/Orders"))
 const Category = lazy(()=>import("../../views/admin/Category"))
 const Sellers = lazy(()=>import("../../views/admin/Sellers"))
 const PaymentRequest = lazy(()=>import("../../views/admin/PaymentRequest"))
+const DeactiveSellers = lazy(()=>import("../../views/admin/DeactiveSellers"))
 
 export const adminRoutes = [
     {
@@ -29,6 +30,11 @@ export const adminRoutes = [
     {
         path: "/admin/dashboard/payment-request",
         element: <PaymentRequest/>,
+        role:'admin'
+    },
+    {
+        path: "/admin/dashboard/deactive-sellers",
+        element: <DeactiveSellers/>,
         role:'admin'
     }
 ]
--- dashboard/src/views/admin/DeactiveSellers.jsx
+++ dashboard/src/views/admin/DeactiveSellers.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import Pagination from '../Pagination' // Adjust the import path if needed
 import { FaEye } from 'react-icons/fa';
 
-const Sellers = () => {
+const DeactiveSellers = () => {
 
     const [currentPage, setCurrentPage] = useState(1);
     const [perPage, setPerPage] = useState(5);
@@ -12,7 +12,7 @@ const Sellers = () => {
 
     return (
         <div className='px-2 lg:px-7 pt-5'>
-        <h1 className='text-[25px] font-semibold text-lg mb-2'>Seller</h1>
+            <h1 className='text-[25px] font-semibold text-lg mb-2'>Decative Sellers</h1>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
                         <div className='flex justify-between items-center'>
                             <select onChange={(e) => setPerPage(parseInt(e.target.value))} className='px-3 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
@@ -30,11 +30,9 @@ const Sellers = () => {
                                         <th scope='col' className='px-4 py-3'>No</th>
                                         <th scope='col' className='px-4 py-3'>Image</th>
                                         <th scope='col' className='px-4 py-3'>Name</th>
-                                        <th scope='col' className='px-4 py-3'>Shop Name</th>
-                                        <th scope='col' className='px-4 py-3'>Payment Status</th>
                                         <th scope='col' className='px-4 py-3'>Email</th>
-                                        <th scope='col' className='px-4 py-3'>Division</th>
-                                        <th scope='col' className='px-4 py-3'>District</th>
+                                        <th scope='col' className='px-4 py-3'>Payment Status</th>
+                                        <th scope='col' className='px-4 py-3'>Status</th>
                                         <th scope='col' className='px-4 py-3'>Action</th>
                                     </tr>
                                 </thead>
@@ -47,11 +45,9 @@ const Sellers = () => {
                                                     <img className='w-[40px] h-[40px] rounded' src={`http://localhost:3000/images/category/${item}.jpg`} alt='category'/>
                                                 </td>
                                                 <td className='px-4 py-1 font-medium whitespace-nowrap'>Abul Kalam Azad</td>
-                                                <td className='px-4 py-1 font-medium whitespace-nowrap'>Coder Shop</td>
-                                                <td className='px-4 py-1 font-medium whitespace-nowrap'>Pending</td>
                                                 <td className='px-4 py-1 font-medium whitespace-nowrap'>rubelazad123@gmail.com</td>
-                                                <td className='px-4 py-1 font-medium whitespace-nowrap'>Rajshahi</td>
-                                                <td className='px-4 py-1 font-medium whitespace-nowrap'>Pabna</td>
+                                                <td className='px-4 py-1 font-medium whitespace-nowrap'>Pending</td>
+                                                <td className='px-4 py-1 font-medium whitespace-nowrap'>De-active</td>
                                                 <td className='px-4 py-1 font-medium whitespace-nowrap'>
                                                     <div className='flex justify-start items-center gap-4'>
                                                     <Link className='p-[6px] bg-green-500 rounded-md hover:shadow-lg hover:shadow-green-500/50'><FaEye/></Link>
@@ -74,4 +70,4 @@ const Sellers = () => {
     );
 };
 
-export default Sellers;
+export default DeactiveSellers;
