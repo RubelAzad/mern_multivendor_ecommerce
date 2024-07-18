6a7bec10f9af02afee12ac223a540dbea3debd2e
RubelAzad <rubelazad123@gmail.com>
2024-07-18 11:45:19 +0600

    individual search component done

---
+++ dashboard/src/views/components/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Search = ({setPerPage,setSearchValue,searchValue}) => {
+    return (
+        <div className='flex justify-between items-center'>
+            <select onChange={(e) => setPerPage(parseInt(e.target.value))} className='px-3 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+            </select>
+            <input onChange={(e) => setSearchValue(e.target.value)} value={searchValue} type="search" className='px-3 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' />
+        </div>
+    );
+};
+
+export default Search;
